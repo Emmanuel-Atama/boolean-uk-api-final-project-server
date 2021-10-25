5cc8d33da4cbf67a3d2d7174f005636a5a0d41de
bruce_milliken <brucemilliken98@gmail.com>
2021-10-25 17:01:25 +0100

    Import routers and setup routes in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ const morgan = require("morgan")
 
 const app = express()
 
+/*IMPORT ROUTERS */
+
+const carsRouter = require("./resources/car/router")
+const competitionsRouter = require("./resources/competition/router")
+const driversRouter = require("./resources/driver/router")
+const racetracksRouter = require("./resources/racetrack/router")
+const sponsorsRouter = require("./resources/sponsor/router")
+const teamsRouter = require("./resources/team/router")
+
+
 /* SETUP MIDDLEWARE */
 
 app.disable("x-powered-by")
@@ -16,6 +26,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(morgan("dev"))
 
 /* SETUP ROUTES */
+
+app.use("/cars", carsRouter)
+app.use("/competitions", competitionsRouter)
+app.use("/drivers", driversRouter)
+app.use("/racetracks", racetracksRouter)
+app.use("/sponsors", sponsorsRouter)
+app.use("/teams", teamsRouter)
 
 app.get("*", (req, res) => {
   res.json({ ok: true })
